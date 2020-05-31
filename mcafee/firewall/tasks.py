857f0239c8726cbbda0588920b5d9a7c362fd6da
Janarthanan Kugathasan <jana19930918@gmail.com>
2020-05-31 14:26:41 +0800

    celery files

--- mcafee/firewall/tasks.py
+++ mcafee/firewall/tasks.py
@@ -1,6 +1,9 @@
 import datetime
 import celery
+import subprocess.sys
 
 @celery.decorators.periodic_task(run_every=datetime.timedelta(minutes=1)) # here we assume we want it to be run every 5 mins
 def myTask():
+    execute=subprocess.Popen(["powershell.exe","D:\\Django_Dev\\api-powershell\\mcafee\\Get-Service.ps1"],stdout=sys.stdout)
+    execute.communicate()
     
