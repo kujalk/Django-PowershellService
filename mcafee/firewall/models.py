5335079126ff7d430f4981bfc62b47936a5f8234
Janarthanan Kugathasan <jana19930918@gmail.com>
2020-06-23 21:02:59 +0800

    Improved code readability

--- mcafee/firewall/models.py
+++ mcafee/firewall/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class firewallstatus(models.Model):
-    Date=models.CharField(max_length=30)
-    Service_Name=models.CharField(max_length=50)
-    Display_Name=models.CharField(max_length=50)
-    Status=models.CharField(max_length=20)
+    Date = models.CharField(max_length=30)
+    Service_Name = models.CharField(max_length=50)
+    Display_Name = models.CharField(max_length=50)
+    Status = models.CharField(max_length=20)
 
     def __str__(self):
-        return (str(self.pk)+"_"+self.Date)
+        return str(self.pk) + "_" + self.Date
